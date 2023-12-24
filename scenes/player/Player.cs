698e04087cb3b15a3a82e7bb46c64e06c0110740
Hans Brems <hbrems@Darkon.local>
2023-12-24 03:38:13 +0100

    Update map

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -4,12 +4,12 @@ using System.Drawing;
 
 public partial class Player : CharacterBody2D
 {
-    private const int Speed = 50;
+	private const int Speed = 50;
 
-    public override void _Process(double delta)
-    {
-        var direction = Input.GetVector("left", "right", "up", "down");
-        Velocity = direction * Speed;
-        MoveAndSlide();
-    }
+	public override void _Process(double delta)
+	{
+		var direction = Input.GetVector("left", "right", "up", "down");
+		Velocity = direction * Speed;
+		MoveAndSlide();
+	}
 }
