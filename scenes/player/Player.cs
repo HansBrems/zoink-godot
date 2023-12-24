dc68024ce10f5c6cf5acdb7d392b82d92980344e
Hans Brems <hbrems@Darkon.local>
2023-12-24 15:12:21 +0100

    Allow player to dash

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -1,15 +1,49 @@
 using Godot;
-using System;
-using System.Drawing;
 
 public partial class Player : CharacterBody2D
 {
-	private const int Speed = 50;
+	private Vector2 _direction = new Vector2(0, 0);
+	private int _speed = 50;
+
+	private bool _canDash = true;
+	private bool _isDashing = false;
+	private Timer _dashCooldownTimer;
+	private Timer _dashTimer;
+
+	public override void _Ready()
+	{
+		_dashCooldownTimer = GetNode<Timer>("DashCooldownTimer");
+		_dashTimer = GetNode<Timer>("DashTimer");
+	}
 
 	public override void _Process(double delta)
 	{
-		var direction = Input.GetVector("left", "right", "up", "down");
-		Velocity = direction * Speed;
+		_direction = _isDashing
+			? _direction
+			: Input.GetVector("left", "right", "up", "down");
+		Velocity = _direction * _speed;
 		MoveAndSlide();
+
+		if (_canDash && Input.IsActionPressed("dash")) Dash();
+	}
+
+	private void Dash()
+	{
+		_canDash = false;
+		_isDashing = true;
+		_speed = 150;
+		_dashTimer.Start();
+	}
+
+	private void EnableDashing()
+	{
+		_canDash = true;
+	}
+
+	private void StopDashing()
+	{
+		_speed = 50;
+		_isDashing = false;
+		_dashCooldownTimer.Start();
 	}
 }
