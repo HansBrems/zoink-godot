e2d7b4b4da93b508aa7a066f827257295424501f
Hans Brems <hbrems@Darkon.local>
2023-12-24 02:01:47 +0100

    Various updates

---
+++ scenes/objects/candle/Candle.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+[Tool]
+public partial class Candle : Node2D
+{
+	private PointLight2D _light;
+	private bool _lightAnimating;
+	private readonly Random _random = new Random();
+	
+	[Export]
+	public Color LightColor { get;set; } = new Color(1, 1, 1, 1);
+	
+	public override void _Ready()
+	{
+		_light = GetNode<PointLight2D>("Light");
+		_light.Color = LightColor;
+	}
+
+	public override void _Process(double delta)
+	{
+		Flicker();
+	}
+
+	private void Flicker()
+	{
+		if (!_lightAnimating)
+		{
+			// Calculate a new energy for the light.
+			var newEnergy = _random.NextSingle() * 0.4 + 1.2;
+			
+			// Calculate how long the tween should last.
+			var animationDuration = _random.NextSingle() * 0.1 + 0.3;
+			
+			// Create the tween.
+			var tween = GetTree().CreateTween();
+			tween.TweenProperty(_light, "energy", newEnergy, animationDuration);
+			_lightAnimating = true;
+			
+			// Make it so the tween sets _lightAnimating to false when it's done animating.
+			tween.TweenCallback(Callable.From(() => _lightAnimating = false));
+		}
+	}
+}
--- scenes/player/player.cs
+++ scenes/player/player.cs
@@ -1,6 +1,15 @@
 using Godot;
 using System;
+using System.Drawing;
 
-public partial class player : CharacterBody2D
+public partial class Player : CharacterBody2D
 {
+	private const int Speed = 50;
+
+	public override void _Process(double delta)
+	{
+		var direction = Input.GetVector("left", "right", "up", "down");
+		Velocity = direction * Speed;
+		MoveAndSlide();
+	}
 }
