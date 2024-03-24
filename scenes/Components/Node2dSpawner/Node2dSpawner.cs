ff18dc2e998b0084da901849b95a7730bd945ee7
Hans Brems <hans.brems@longville-software.be>
2024-03-24 17:20:34 +0100

    Refactoring

---
+++ scenes/Components/Node2dSpawner/Node2dSpawner.cs
@@ -0,0 +1,34 @@
+using System;
+using Godot;
+
+namespace Zoink.scenes.Components.EnemySpawner;
+
+public partial class Node2dSpawner : Node2D
+{
+	private Timer _spawnTimer;
+
+	[Export] public PackedScene EnemyScene { get; set; }
+	[Export] public double SpawnRate { get; set; } = 2;
+
+	public override void _Ready()
+	{
+		_spawnTimer = GetNode<Timer>("SpawnTimer");
+		_spawnTimer.WaitTime = SpawnRate;
+		_spawnTimer.Timeout += SpawnEnemy;
+	}
+
+	private void SpawnEnemy()
+	{
+		if (EnemyScene == null)
+			throw new ArgumentException("Missing enemy scene");
+
+		if (EnemyScene.Instantiate() is not Node2D enemy)
+			throw new Exception("Failed to instantiate enemy");
+
+		enemy.Position = Position;
+
+		GetTree().CurrentScene
+            .GetNode<Node2D>("Enemies")
+            .AddChild(enemy);
+	}
+}
